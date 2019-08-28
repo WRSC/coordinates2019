bf832b5f522b9d2bb5e7c28849f7ba27737e93e6
tsaoyu <tsaoyu@tsaoyu.com>
2019-08-28 18:56:02 +0800

    Move fleet race WP

--- utility/csv-2s-converter.py
+++ utility/csv-2s-converter.py
@@ -13,7 +13,7 @@ processed_row = []
 with open(file_name, 'r') as f:
     csv_reader = csv.reader(f)
     for row in csv_reader:
-        processed_row.append('2019-08-26T0' + row[0][:2] + ':' +
+        processed_row.append('2019-08-26T' + row[0][:2] + ':' +
                row[0][2:4] + ':' +  row[0][4:6] + 'Z,' +
                str(float(row[1])/10000000) + ',' + 
                str(float(row[2])/10000000) + ',0,0\n' )
